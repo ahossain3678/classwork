c1a84b02c6f05c995d66f98ff7dc271f191cfd02
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-09-29 15:23:57 -0400

    B

--- Attendance/src/chatbot/Chatbot.java
+++ Attendance/src/chatbot/Chatbot.java
@@ -6,22 +6,27 @@ public class Chatbot {
 	private boolean chatting;
 	private Topic abid;
 	
-	public void Chatbot() {
+	public Chatbot() {
 		abid = new ChatbotAbid();
 		username = "Unknown User";
 		chatting = true;
 	}
+	public String getUsername() {
+		return username;
+	}
+
 	public void startChatting() {
-		ChatbotMain.print("Hi. Tell me your name.");
+		//whenever you print or get input, use these methods
+		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your input. Tell me your name.");
 		username = ChatbotMain.getInput();
 		while(chatting) {
-			ChatbotMain.print("what's up");
+			ChatbotMain.print("What would you like to talk about?");
 			String response = ChatbotMain.getInput();
 			if(abid.isTriggered(response)) {
-				chatting = false; // exit the while loop. 
+				chatting = false;//exits the while loop. IMPORTANT you get graded for this!
 				abid.talk(response);
 			}else {
-				ChatbotMain.print("I'm sorry. I don't understand");
+				ChatbotMain.print("I'm sorry. I don't understand. I never said I was perfect.");
 			}
 		}
 	}
--- Attendance/src/chatbot/ChatbotAbid.java
+++ Attendance/src/chatbot/ChatbotAbid.java
@@ -2,13 +2,40 @@ package chatbot;
 
 public class ChatbotAbid implements Topic{
 
+	private String[] keywords;
+	private String goodbyeKeyword;
+	private String secretKeyword;
+	private String response;
+	
+	public ChatbotAbid() {
+		String [] temp = {"stuff", "things", "whatever", "nothing"};
+		keywords = temp;
+		goodbyeKeyword = "bye";
+		secretKeyword = "pudding";
+		response = "";
+	}
 	public void talk(String response) {
-
-		
+		ChatbotMain.print("You want to talk about that? Tell me about it.");
+		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) != -1) {
+			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0 ) {
+				ChatbotMain.print("OMG LET ME GET SOME PUDDING. I LOVEEE PUDDDING");
+				response = ChatbotMain.getInput();
+			}else {
+				ChatbotMain.print("Yeah that's great. Tell me something interesting");
+				response = ChatbotMain.getInput();
+			}
+		}
+		// access other classes
+		ChatbotMain.print("Well it was nice talking to you, "+ChatbotMain.chatbot.getUsername()+"!");
+		ChatbotMain.chatbot.startChatting();
 	}
 
 	public boolean isTriggered(String response) {
-
+		for(int i =0; i < keywords.length; i++) {
+			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0) {
+				return true;
+			}
+		}
 		return false;
 	}
 
